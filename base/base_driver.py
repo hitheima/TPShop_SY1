6d8c1ec614353e8ddd1c7280cc12ab06c3f221de
hitheima <hitheima@163.com>
2018-11-15 15:09:26 +0800

    跳转到登录页面

--- base/base_driver.py
+++ base/base_driver.py
@@ -7,10 +7,8 @@ def init_driver():
     desired_caps['platformName'] = 'Android'
     desired_caps['platformVersion'] = '5.1'
     desired_caps['deviceName'] = '192.168.56.101:5555'
-    desired_caps['appPackage'] = 'com.android.contacts'
-    desired_caps['appActivity'] = '.activities.PeopleActivity'
-    desired_caps['noReset'] = True
-
+    desired_caps['appPackage'] = 'com.tpshop.malls'
+    desired_caps['appActivity'] = '.SPMainActivity'
 
     # 声明driver对象
     driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,4 +1,26 @@
+import time
+
+from base.base_driver import init_driver
+from page.page import Page
+
+
 class TestLogin:
 
+    def setup(self):
+        self.driver = init_driver()
+        self.page = Page(self.driver)
+
+    def teardown(self):
+        time.sleep(5)
+        self.driver.quit()
+
     def test_login(self):
-        print("test_login")
+        # 首页-我的
+        self.page.home.click_mine_button()
+        # 我的-登录/注册
+        self.page.mine.click_login_and_sign_up()
+
+        # 输入用户名
+        # 输入密码
+        # 点击登录
+
