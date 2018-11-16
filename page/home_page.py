c3fe7b3d2c5d2ae6c37fa0c290b7a346c3e6b3f4
hitheima <hitheima@163.com>
2018-11-16 10:37:21 +0800

    增加登录的报告信息

--- page/home_page.py
+++ page/home_page.py
@@ -1,11 +1,13 @@
 from selenium.webdriver.common.by import By
 
 from base.base_action import BaseAction
+import allure
 
 
 class HomePage(BaseAction):
 
     mine_button = By.XPATH, "//*[@text='我的' and @resource-id='com.tpshop.malls:id/tab_txtv']"
 
+    @allure.step("首页页面-点击-我的按钮")
     def click_mine_button(self):
         self.click(self.mine_button)
--- page/login_page.py
+++ page/login_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 
 from base.base_action import BaseAction
+import allure
 
 
 class LoginPage(BaseAction):
@@ -11,11 +12,16 @@ class LoginPage(BaseAction):
 
     login_button = By.ID, "com.tpshop.malls:id/btn_login"
 
+    @allure.step("登录页面-输入-用户名")
     def input_username(self, text):
+        allure.attach("用户名", text)
         self.input(self.username_edit_text, text)
 
+    @allure.step("登录页面-输入-密码")
     def input_password(self, text):
+        allure.attach("密码", text)
         self.input(self.password_edit_text, text)
 
+    @allure.step("登录页面-点击-登录按钮")
     def click_login(self):
         self.click(self.login_button)
