5e29460b343066d7784eff8aa080ef21ed9d21b8
hitheima <hitheima@163.com>
2018-11-16 09:35:31 +0800

    修改toast的find的默认时间

--- base/base_action.py
+++ base/base_action.py
@@ -7,13 +7,13 @@ class BaseAction:
     def __init__(self, driver):
         self.driver = driver
 
-    def find_element(self, feature, timeout=10, poll=1):
+    def find_element(self, feature, timeout=10.0, poll=1.0):
         by = feature[0]
         value = feature[1]
 
         return WebDriverWait(self.driver, timeout, poll).until(lambda x: x.find_element(by, value))
 
-    def find_elements(self, feature, timeout=10, poll=1):
+    def find_elements(self, feature, timeout=10.0, poll=1.0):
         by = feature[0]
         value = feature[1]
 
@@ -30,7 +30,7 @@ class BaseAction:
 
     def is_toast_exist(self, text):
         try:
-            self.find_element((By.XPATH, "//*[contains(@text,'%s')]" % text))
+            self.find_element((By.XPATH, "//*[contains(@text,'%s')]" % text), timeout=5, poll=0.5)
             return True
         except Exception:
             return False
