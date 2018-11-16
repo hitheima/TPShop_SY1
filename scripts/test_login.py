ab8b28b3f3aa0bc90ceb5ff5515a0f3ff094c572
hitheima <hitheima@163.com>
2018-11-16 10:22:39 +0800

    登录脚本的参数化

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,7 +1,10 @@
 import time
 
+import pytest
+
 from base.base_driver import init_driver
 from page.page import Page
+from base.base_analyze import analyze_file
 
 
 class TestLogin:
@@ -14,24 +17,24 @@ class TestLogin:
         time.sleep(5)
         self.driver.quit()
 
-    def test_login(self):
+    @pytest.mark.parametrize("args", analyze_file("login_data.yml", "test_login"))
+    def test_login(self, args):
+        # 准备数据
+        username = args["username"]
+        password = args["password"]
+        hint = args["hint"]
+
         # 首页-我的
         self.page.home.click_mine_button()
         # 我的-登录/注册
         self.page.mine.click_login_and_sign_up()
         # 登录-输入用户名
-        self.page.login.input_username("13800138006")
+        self.page.login.input_username(username)
         # 登录-输入密码
-        self.page.login.input_password("123456")
+        self.page.login.input_password(password)
         # 登录-点击登录
         self.page.login.click_login()
 
-        # res =
-
-        # if res:
-        #     assert True
-        # else:
-        #     assert False
-
-        assert self.page.login.is_toast_exist("登录成功")
+        # 断言
+        assert self.page.login.is_toast_exist(hint)
 
