69c80e5bd61d6848af8eeb70e336b4bffc98112f
hitheima <hitheima@163.com>
2018-11-18 09:50:25 +0800

    没有登录的时候先登录

--- scripts/test_address.py
+++ scripts/test_address.py
@@ -1,3 +1,5 @@
+import time
+
 from base.base_driver import init_driver
 from page.page import Page
 
@@ -15,13 +17,18 @@ class TestAddress:
         self.page.home.click_mine_button()
 
         if not self.page.mine.is_login():
-            print("登录")
-            # 登录
-            # 输入用户名
-            # 输入xx
-            # 点登录
+            # 我的-登录/注册
+            self.page.mine.click_login_and_sign_up()
+            # 登录-输入用户名
+            self.page.login.input_username("13800138006")
+            # 登录-输入密码
+            self.page.login.input_password("123456")
+            # 登录-点击登录
+            self.page.login.click_login()
 
         print("收货地址")
+
+        time.sleep(10)
         # 新建地址
         # 添加联系人
         # 添加手机
